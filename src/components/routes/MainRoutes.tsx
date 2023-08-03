a1fd173c8fb827ce11ddf6b886361138309f6c3d
Hilel Kinane <hilel.kinane@gmail.com>
2023-08-04 00:47:59 +0200

    deploy

--- src/components/routes/MainRoutes.tsx
+++ src/components/routes/MainRoutes.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { UserAuth, useUserAuth } from "../../contexts/AuthContext"
 import LogRoutes from "./LogRoutes";
 import ProtectedRoutes from "./ProtectedRoutes";
@@ -6,18 +5,6 @@ import ProtectedRoutes from "./ProtectedRoutes";
 
 export const MainRoutes = () => {
     const { userLogged } = useUserAuth() as UserAuth;
-    // const [userChanged, setUserChanged] = useState(false);
-
-    // // Utilisez useEffect pour mettre à jour userChanged à chaque changement de l'utilisateur
-    // useEffect(() => {
-    //     setUserChanged(true);
-    // }, [userLogged]);
-
-    // // Utilisez useEffect avec une dépendance vide pour mettre à jour localement user
-    // useEffect(() => {
-    //     setUserChanged(false);
-    // }, []);
-
 
     if (!userLogged) {
         return <LogRoutes />;
